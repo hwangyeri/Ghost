1332120e1d6ecc4d57ad262c5ec3ec11e17f9c7c
hwangyeri <dpfl420@naver.com>
2023-11-16 17:11:47 +0900

    [Chore] 프로젝트 파일 생성

---
+++ Blanky/Blanky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
