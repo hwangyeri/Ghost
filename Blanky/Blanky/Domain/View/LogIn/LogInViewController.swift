e1acc82e6430cc47845d999a5b6e607dc42e139c
hwangyeri <dpfl420@naver.com>
2023-11-18 02:38:01 +0900

    [Refactor] RootView - CustomView로 교체, BaseViewController 채택

--- Blanky/Blanky/Domain/View/LogIn/LogInViewController.swift
+++ Blanky/Blanky/Domain/View/LogIn/LogInViewController.swift
@@ -7,23 +7,19 @@
 
 import UIKit
 
-class LogInViewController: UIViewController {
+class LogInViewController: BaseViewController {
+    
+    private let mainView = LogInView()
+    
+    override func loadView() {
+        self.view = mainView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+//        view.backgroundColor = .brown
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
