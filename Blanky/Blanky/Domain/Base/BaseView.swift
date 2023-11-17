bd2979110bccdf64b118ecb85a552ac3de7d698d
hwangyeri <dpfl420@naver.com>
2023-11-17 21:21:41 +0900

    [Rename] Domain/Base 그룹으로 폴더링 및 BaseView 생성

---
+++ Blanky/Blanky/Domain/Base/BaseView.swift
@@ -0,0 +1,28 @@
+//
+//  BaseView.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/11/17.
+//
+
+import UIKit
+
+class BaseView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() { }
+    
+    func configureLayout() { }
+    
+}
+
+
