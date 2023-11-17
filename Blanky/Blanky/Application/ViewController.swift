964c71d07f2e342208614168394586e31cc7987f
hwangyeri <dpfl420@naver.com>
2023-11-18 02:38:30 +0900

    [Test] 테스트 코드 작성

--- Blanky/Blanky/Application/ViewController.swift
+++ Blanky/Blanky/Application/ViewController.swift
@@ -6,12 +6,44 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    let mainLabel = {
+        let view = BLabel(text: "dddd", fontWeight: .black, fontSize: .XXXL)
+        view.textColor = .red
+        return view
+    }()
+    
+    let emailTextField = {
+        let view = UITextField()
+//        view.placeholder = "예) blanky@blanky.com"
+        view.attributedPlaceholder = NSAttributedString(string: "예) blanky@blanky.com", attributes: [NSAttributedString.Key.foregroundColor: UIColor.systemGray2])
+        view.font = .customFont(.regular, size: .S)
+//        view.textColor = .black
+//        view.borderStyle = .line
+//        view.layer.borderColor = UIColor(ciColor: .black).cgColor
+        view.autocapitalizationType = .none // 자동 대문자
+        view.spellCheckingType = .no // 맞춤법 검사
+        view.autocorrectionType = .no // 자동 수정
+        view.keyboardType = .emailAddress
+        view.clearButtonMode = .whileEditing
+        view.clearsOnBeginEditing = true // 편집 시 기존 텍스트필드값 제거
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        view.addSubview(emailTextField)
+        emailTextField.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.height.equalTo(60)
+        }
     }
 
 
