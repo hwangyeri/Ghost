7e47016bb778391cd7a4b35ff06f4962660135ed
hwangyeri <dpfl420@naver.com>
2023-11-18 02:11:58 +0900

    [Feat] Utility/DesignSystem/Component 그룹으로 폴더링 및 BDivider 생성

---
+++ Blanky/Blanky/Utility/DesignSystem/Component/BDivider.swift
@@ -0,0 +1,22 @@
+//
+//  BDivider.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/11/18.
+//
+
+import UIKit
+
+final class BDivider: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+       
+        self.backgroundColor = .darkGray
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
