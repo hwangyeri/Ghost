ae63290807cbed4990992b492bee35864a49033e
hwangyeri <dpfl420@naver.com>
2023-11-17 22:40:28 +0900

    [Feat] Utility/DesignSystem/Extension 그룹으로 폴더링 및 Font+ 파일 생성

---
+++ Blanky/Blanky/Utility/DesignSystem/Extension/Font+.swift
@@ -0,0 +1,38 @@
+//
+//  Font+.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/11/17.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    enum CustomFontWeight: String {
+        case black = "Black"
+        case bold = "Bold"
+        case light = "Light"
+        case medium = "Medium"
+        case regular = "Regular"
+        case semiBold = "SemiBold"
+        case thin = "Thin"
+    }
+    
+    enum CustomFontSize: CGFloat {
+        case XXS = 12
+        case XS = 13
+        case S = 14
+        case M = 15
+        case L = 16
+        case XL = 18
+        case XXL = 20
+        case XXXL = 24
+    }
+    
+    static func customFont(_ weight: CustomFontWeight, size: CustomFontSize) -> UIFont? {
+        let fontName = "Pretendard-\(weight.rawValue)"
+        return UIFont(name: fontName, size: size.rawValue)
+    }
+}
+
