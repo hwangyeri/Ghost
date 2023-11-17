70132460035e037c6868c04953df8a20b6cc2b14
hwangyeri <dpfl420@naver.com>
2023-11-17 21:22:38 +0900

    [Rename] Domain/Base 그룹으로 폴더링 및 BaseViewController 생성

---
+++ Blanky/Blanky/Domain/Base/BaseViewController.swift
@@ -0,0 +1,23 @@
+//
+//  BaseViewController.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/11/17.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureHierarchy()
+        configureLayout()
+    }
+    
+    func configureHierarchy() { }
+    
+    func configureLayout() { }
+    
+}
+
