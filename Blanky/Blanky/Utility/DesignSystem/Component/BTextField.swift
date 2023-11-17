d80682531374606799263ab814e227e6bda2ea65
hwangyeri <dpfl420@naver.com>
2023-11-18 02:13:21 +0900

    [Feat] Utility/DesignSystem/Component 그룹으로 폴더링 및 BTextField 생성

---
+++ Blanky/Blanky/Utility/DesignSystem/Component/BTextField.swift
@@ -0,0 +1,31 @@
+//
+//  BTextField.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/11/18.
+//
+
+import UIKit
+
+final class BTextField: UITextField {
+    
+    init(keyboardType: UIKeyboardType, returnKeyType: UIReturnKeyType) {
+        super.init(frame: .zero)
+        
+        self.font = .customFont(.regular, size: .S)
+        self.textColor = .white
+        self.autocapitalizationType = .none // 자동 대문자
+        self.autocorrectionType = .no // 자동 수정
+        self.spellCheckingType = .no // 맞춤법 검사
+        self.clearButtonMode = .whileEditing
+        self.clearsOnBeginEditing = true // 편집 시 기존 텍스트필드값 제거
+        self.keyboardType = keyboardType
+        self.returnKeyType = returnKeyType
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
