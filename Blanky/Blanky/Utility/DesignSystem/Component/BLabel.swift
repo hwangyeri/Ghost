10a5ec806f107ae95bc920583abe69a3541a5dd3
hwangyeri <dpfl420@naver.com>
2023-11-18 02:12:27 +0900

    [Feat] Utility/DesignSystem/Component 그룹으로 폴더링 및 BLabel 생성

---
+++ Blanky/Blanky/Utility/DesignSystem/Component/BLabel.swift
@@ -0,0 +1,25 @@
+//
+//  BLabel.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/11/18.
+//
+
+import UIKit
+
+final class BLabel: UILabel {
+    
+    init(text: String, fontWeight: UIFont.CustomFontWeight, fontSize: UIFont.CustomFontSize) {
+        super.init(frame: .zero)
+       
+        self.text = text
+        self.font = .customFont(fontWeight, size: fontSize)
+        self.textColor = .white
+        self.numberOfLines = 0
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
