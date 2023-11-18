d23091a99798e51f62d287a702c11c859986a519
hwangyeri <dpfl420@naver.com>
2023-11-18 16:55:20 +0900

    [Refactor] EmailStepVC - RootView 교체, BaseViewController 채택 및 파일명 변경
    - JoinViewController -> EmailStepViewController

---
+++ Blanky/Blanky/Domain/View/Join/EmailStepViewController.swift
@@ -0,0 +1,24 @@
+//
+//  JoinViewController.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/11/16.
+//
+
+import UIKit
+
+class EmailStepViewController: BaseViewController {
+    
+    private let mainView = EmailStepView()
+    
+    override func loadView() {
+        self.view = mainView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
