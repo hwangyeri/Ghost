386d592bf9b5feaacfb38aecdd01dde4a4a8ca26
hwangyeri <dpfl420@naver.com>
2023-11-18 16:18:07 +0900

    [Feat] Utility/DesignSystem/Component 그룹으로 폴더링 및 BTextButton 생성

---
+++ Blanky/Blanky/Utility/DesignSystem/Component/BTextButton.swift
@@ -0,0 +1,25 @@
+//
+//  BTextButton.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/11/18.
+//
+
+import UIKit
+
+final class BTextButton: UIButton {
+    
+    init(text: String) {
+        super.init(frame: .zero)
+        
+        self.setTitle(text, for: .normal)
+        self.titleLabel?.font = .customFont(.medium, size: .S)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+
