6be3c064d3362feff4ce2d0e7f740b04fd0c3d61
hwangyeri <dpfl420@naver.com>
2023-11-19 16:16:02 +0900

    [Feat] JoinStep2View 생성 및 초기 설정

---
+++ Blanky/Blanky/Domain/View/Join/JoinStep2View.swift
@@ -0,0 +1,21 @@
+//
+//  PasswordStepView.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/11/18.
+//
+
+import UIKit
+import SnapKit
+
+class JoinStep2View: BaseView {
+    
+    override func configureHierarchy() {
+        
+    }
+    
+    override func configureLayout() {
+        
+    }
+    
+}
