720e58edd0a6433ff0cec3d8fafb897f31f9a16d
hwangyeri <dpfl420@naver.com>
2023-11-19 21:49:19 +0900

    [Feat] 가입 1단계 화면 - 유효성 검사에 따른 UI 업데이트 구현
    - import RxSwift, RxCocoa
    - viewModel, disposeBag 인스턴스 생성
    - bind 메서드 생성
    - checkDuplicationButton addTarget, 메서드 생성

--- Blanky/Blanky/Domain/View/Join/JoinStep1/JoinStep1ViewController.swift
+++ Blanky/Blanky/Domain/View/Join/JoinStep1/JoinStep1ViewController.swift
@@ -6,10 +6,16 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class JoinStep1ViewController: BaseViewController {
     
     private let mainView = JoinStep1View()
+    
+    private let viewModel = JoinStep1ViewModel()
+    
+    let disposeBag = DisposeBag()
     
     override func loadView() {
         self.view = mainView
@@ -18,10 +24,50 @@ class JoinStep1ViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        bind()
     }
     
     override func configureLayout() {
         self.navigationItem.title = "회원가입"
+        mainView.checkDuplicationButton.addTarget(self, action: #selector(checkDuplicationButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func checkDuplicationButtonTapped() {
+        print(#function)
+    }
+    
+    private func bind() {
+        
+        let input = JoinStep1ViewModel.Input(
+            emailTextField: mainView.emailTextField.rx.text.orEmpty,
+            passwordTextField: mainView.passwordTextField.rx.text.orEmpty,
+            checkPasswordTextField: mainView.checkPasswordTextField.rx.text.orEmpty,
+            checkDuplicationButton: mainView.checkDuplicationButton.rx.tap,
+            nextButton: mainView.nextButton.rx.tap)
+        
+        let output = viewModel.transform(input: input)
+        
+        output.emailValidation
+            .drive(with: self) { owner, isValid in
+                owner.mainView.emailTextField.borderActiveColor = isValid ? .point : .systemPink
+                owner.mainView.checkDuplicationButton.isEnabled = isValid
+                owner.mainView.checkDuplicationButton.backgroundColor = isValid ? .white : .gray
+            }
+            .disposed(by: disposeBag)
+        
+        output.passwordValidation
+            .drive(with: self) { owner, isValid in
+                owner.mainView.passwordTextField.borderActiveColor = isValid ? .point : .systemPink
+            }
+            .disposed(by: disposeBag)
+        
+        output.checkPasswordValidation
+            .drive(with: self) { owner, isValid in
+                owner.mainView.checkPasswordTextField.borderActiveColor = isValid ? .point : .systemPink
+
+            }
+            .disposed(by: disposeBag)
+        
     }
 
 }
