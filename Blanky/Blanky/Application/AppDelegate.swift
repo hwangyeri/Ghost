f25c4c2bcb1669e6f552300aa5b6beef8c6a6aec
hwangyeri <dpfl420@naver.com>
2023-11-19 23:35:04 +0900

    [Feat] AppDelegate - IQKeyboardManagerSwift 설정

--- Blanky/Blanky/Application/AppDelegate.swift
+++ Blanky/Blanky/Application/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = true
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+        IQKeyboardManager.shared.toolbarTintColor = .point
+        IQKeyboardManager.shared.toolbarDoneBarButtonItemText = "확인"
+        
         return true
     }
 
