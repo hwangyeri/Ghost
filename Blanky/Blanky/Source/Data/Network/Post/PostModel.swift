fb95a7441acac206b61b166a9264049c71618862
hwangyeri <dpfl420@naver.com>
2023-11-21 16:53:57 +0900

    [Feat] Source/Data/Network/Post 그룹으로 폴더링 및 PostModel 생성

---
+++ Blanky/Blanky/Source/Data/Network/Post/PostModel.swift
@@ -0,0 +1,42 @@
+//
+//  PostModel.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/11/20.
+//
+
+import Foundation
+
+// MARK: 회원 가입
+struct JoinInput: Encodable {
+    let email: String
+    let password: String
+    let nick: String
+}
+
+struct JoinOutput: Decodable {
+    let id: String
+    let email: String
+    let nick: String
+}
+
+// MARK: 이메일 중복 확인
+struct ValidationEmailInput: Encodable {
+    let email: String
+}
+
+struct ValidationEmailOutput: Decodable {
+    let message: String
+}
+
+// MARK: 로그인
+struct LoginInput: Encodable {
+    let email: String
+    let password: String
+}
+
+struct LoginOutput: Decodable {
+    let token: String
+    let refreshToken: String
+}
+
