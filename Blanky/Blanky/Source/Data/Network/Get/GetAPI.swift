50b01b115d9975c43a2bd835b87d1ee8a8453393
hwangyeri <dpfl420@naver.com>
2023-11-21 17:25:53 +0900

    [Feat] Source/Data/Network/Get 그룹으로 폴더링 및 GetAPI 생성 (진행중)

---
+++ Blanky/Blanky/Source/Data/Network/Get/GetAPI.swift
@@ -0,0 +1,55 @@
+//
+//  GetAPI.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/11/21.
+//
+
+import Foundation
+import Moya
+
+enum GetAPI: String {
+    case refresh // AcessToken 갱신
+    case withdraw // 탈퇴
+}
+
+extension GetAPI: TargetType {
+    
+    var baseURL: URL {
+        return URL(string: APIKey.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .refresh, .withdraw:
+            return rawValue
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .refresh, .withdraw:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .refresh, .withdraw:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .refresh:
+            ["Authorization": "", // FIXME
+             "SesacKey": APIKey.sesacKey,
+             "Refresh": "refreshToken"]
+        case .withdraw:
+            ["Authorization": "", // FIXME
+             "SesacKey": APIKey.sesacKey]
+        }
+    }
+    
+}
