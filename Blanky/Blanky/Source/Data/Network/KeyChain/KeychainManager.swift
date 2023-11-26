a970f748dc6d0061bcbf6747ea3231507bdcda27
hwangyeri <dpfl420@naver.com>
2023-11-27 01:06:46 +0900

    [Feat] Source/Data/Network/KeyChain 그룹으로 폴더링 및 KeychainManager 생성 (진행중)

---
+++ Blanky/Blanky/Source/Data/Network/KeyChain/KeychainManager.swift
@@ -0,0 +1,18 @@
+//
+//  KeychainManager.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/11/27.
+//
+
+import Foundation
+import Security
+
+final class KeychainManager {
+    
+    static let shared = KeychainManager()
+    
+    private init() {}
+    
+   
+}
