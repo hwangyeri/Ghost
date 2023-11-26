afc553ac7155e55ed9e921047efa8148a7cd8b53
hwangyeri <dpfl420@naver.com>
2023-11-26 21:11:40 +0900

    [Feat] Source/Utility/DesignSystem/Extension 그룹으로 폴더링 및 aviBarButton+ 생성
    - Custom NavigationBarItem(Left) BackButton : 이전 뷰로 화면 전환
    - Custom NavigationBarItem(Right) ExitButton 나가기 버튼 : 루트 뷰로 화면 전환

---
+++ Blanky/Blanky/Source/Utility/DesignSystem/Extension/NaviBarButton+.swift
@@ -0,0 +1,56 @@
+//
+//  NaviBarButton+.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/11/26.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+extension UIViewController {
+    
+    //MARK: Custom NavigationBarItem(Left) - BackButton
+    func setCustomBackButton() {
+        let backButton = BImageButton(imageSize: 18, imageName: "chevron.backward", backgroundColor: .bColor200, tintColor: .white, cornerRadius: 15)
+        
+        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        
+        backButton.snp.makeConstraints { make in
+            make.size.equalTo(44)
+        }
+        
+        
+        let customBackButton = UIBarButtonItem(customView: backButton)
+        
+        self.navigationItem.leftBarButtonItem = customBackButton
+    }
+    
+    //이전 뷰로 화면 전환
+    @objc func backButtonTapped() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    //MARK: Custom NavigationBarItem(Right) - ExitButton 나가기 버튼
+    func setCustomExitButton() {
+        let exitButton = BImageButton(imageSize: 16, imageName: "xmark", backgroundColor: .bColor200, tintColor: .white, cornerRadius: 15)
+        
+        exitButton.addTarget(self, action: #selector(exitButtonTapped), for: .touchUpInside)
+        
+        exitButton.snp.makeConstraints { make in
+            make.size.equalTo(44)
+        }
+        
+        let customExitButton = UIBarButtonItem(customView: exitButton)
+        
+        self.navigationItem.rightBarButtonItem = customExitButton
+    }
+    
+    
+    //루트뷰로 화면 전환
+    @objc func exitButtonTapped() {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+}
