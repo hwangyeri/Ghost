41041c85f5582d81720ce76ba69ff2c5dc6b57ec
hwangyeri <dpfl420@naver.com>
2023-11-27 01:04:07 +0900

    [Feat] MVVM / RxSwift / Input-Output 구조로 회원가입(2) 화면 구현
    - userInfo 값 전달받아서 회원가입 API 요청
    - 닉네임 유효성 검사 및 UI 업데이트
    - 랜덤 버튼 클릭 시 랜덤 닉네임 추천 기능 구현 및 UI 업데이트
    - 다음 버튼 클릭 시 회원가입 API 통신해서 회원가입 응답 처리, 예외처리 및 화면 전환

--- Blanky/Blanky/Source/Domain/View/Join/JoinStep2/JoinStep2ViewController.swift
+++ Blanky/Blanky/Source/Domain/View/Join/JoinStep2/JoinStep2ViewController.swift
@@ -6,18 +6,74 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class JoinStep2ViewController: BaseViewController {
     
     private let mainView = JoinStep2View()
     
+    private let viewModel = JoinStep2ViewModel()
+    
+    var userInfo: JoinInput?
+    
+    let disposeBag = DisposeBag()
+    
     override func loadView() {
         self.view = mainView
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        bind()
+    }
+    
+    override func configureLayout() {
+        self.navigationItem.title = "회원가입"
+        setCustomBackButton()
+        setCustomExitButton()
+    }
+    
+    private func bind() {
+        
+        let input = JoinStep2ViewModel.Input(
+            useInfo: userInfo ?? JoinInput(email: "", password: "", nick: ""),
+            nicknameTextField: mainView.nicknameTextField.rx.text.orEmpty,
+            randomButton: mainView.randomButton.rx.tap,
+            doneButton: mainView.doneButton.rx.tap)
+        
+        let output = viewModel.transform(input: input)
+        
+        output.nicknameValidation
+            .drive(with: self) { owner, isValid in
+                owner.mainView.nicknameTextField.borderActiveColor = isValid ? .point : .systemPink
+                owner.mainView.doneButton.isEnabled = isValid
+                owner.mainView.doneButton.backgroundColor = isValid ? .white : .gray
+            }
+            .disposed(by: disposeBag)
+        
+        output.randomButtonTap
+            .drive(with: self) { owner, nickname in
+                owner.mainView.nicknameTextField.text = nickname
+                owner.mainView.nicknameTextField.borderActiveColor = .point
+                owner.mainView.doneButton.isEnabled = true
+                owner.mainView.doneButton.backgroundColor = .white
+            }
+            .disposed(by: disposeBag)
+        
+        output.doneButtonTap
+            .drive(with: self) { owner, result in
+                print(result)
+                switch result {
+                case true:
+                    let vc = ViewController() // FIXME
+                    owner.navigationController?.pushViewController(vc, animated: true)
+                case false:
+                    owner.showAlertMessage(title: "", message: "다시 시도해 주세요.")
+                }
+            }
+            .disposed(by: disposeBag)
     }
     
 }
