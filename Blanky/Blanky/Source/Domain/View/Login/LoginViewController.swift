ab0aafd3701f404a2021dab86538d3c60d4dd620
hwangyeri <dpfl420@naver.com>
2023-11-27 20:02:22 +0900

    [Feat] MVVM / RxSwift / Input-Output 구조로 로그인 화면 기능 구현 및 UI 업데이트
    - 이메일/비밀번호 유효성 검사 및 UI 업데이트 기능 구현
    - 로그인 버튼 클릭 시 로그인 API 응답 처리 및 화면 전환 기능 구현
    - 회원가입 버튼 클릭시 화면 전환 기능 구현
    - notification 신호 받아서 Custom Alert 기능 구현
    - 이전 화원가입 메서드 제거

--- Blanky/Blanky/Source/Domain/View/Login/LoginViewController.swift
+++ Blanky/Blanky/Source/Domain/View/Login/LoginViewController.swift
@@ -6,10 +6,16 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class LoginViewController: BaseViewController {
     
     private let mainView = LoginView()
+    
+    private let viewModel = LoginViewModel()
+    
+    let disposeBag = DisposeBag()
     
     override func loadView() {
         self.view = mainView
@@ -18,16 +24,67 @@ class LoginViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        bind()
     }
     
-    override func configureLayout() {
-        mainView.joinButton.addTarget(self, action: #selector(joinButtonTapped), for: .touchUpInside)
+    private func bind() {
+        
+        let input = LoginViewModel.Input(
+            emailTextField: mainView.emailTextField.rx.text.orEmpty,
+            passwordTextField: mainView.passwordTextField.rx.text.orEmpty,
+            loginButton: mainView.loginButton.rx.tap,
+            findEmailButton: mainView.findEmailButton.rx.tap,
+            findPasswordButton: mainView.findPasswordButton.rx.tap,
+            joinButton: mainView.joinButton.rx.tap)
+        
+        let output = viewModel.transform(input: input)
+        
+        output.emailValidation
+            .drive(with: self) { owner, isValid in
+                owner.mainView.emailTextField.borderActiveColor = isValid ? .point : .systemPink
+            }
+            .disposed(by: disposeBag)
+        
+        output.passwordValidation
+            .drive(with: self) { owner, isValid in
+                owner.mainView.passwordTextField.borderActiveColor = isValid ? .point : .systemPink
+            }
+            .disposed(by: disposeBag)
+        
+        output.finalValidation
+            .drive(with: self) { owner, isValid in
+                owner.mainView.loginButton.isEnabled = isValid
+                owner.mainView.loginButton.backgroundColor = isValid ? .white : .gray
+            }
+            .disposed(by: disposeBag)
+        
+        output.loginButtonTap
+            .drive(with: self) { owner, result in
+                switch result.0 {
+                case true:
+                    let vc = ViewController()
+                    owner.navigationController?.pushViewController(vc, animated: true)
+                case false:
+                    owner.showAlertMessage(title: "", message: result.1)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        output.joinButtonTap
+            .drive(with: self) { owner, _ in
+                let vc = JoinStep1ViewController()
+                owner.navigationController?.pushViewController(vc, animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+        NotificationCenter.default.rx.notification(.joinSuccessAlert)
+            .observeOn(MainScheduler.instance)
+            .subscribe(with: self) { owner, notification in
+                if let email = notification.userInfo?["email"] as? String {
+                    owner.showAlertMessage(title: email, message: "성공적으로 가입되었습니다.\n가입한 이메일로 로그인해주세요.")
+                }
+            }
+            .disposed(by: disposeBag)
     }
     
-    // MARK: 회원가입 버튼
-    @objc private func joinButtonTapped() {
-        print(#function)
-        let vc = JoinStep1ViewController()
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
 }
