95d5242bed3495256d20ea5ef01cd5a125871f35
hwangyeri <dpfl420@naver.com>
2023-11-27 19:35:52 +0900

    [Feat] MVVM / RxSwift / Input-Output 구조로 LoginViewModel 구현
    - 이메일/비밀번호 유효성 검사
    - 로그인 버튼 클릭 시 로그인 API 응답 처리
    - 회원가입 버튼 클릭시 화면전환 기능 구현

--- Blanky/Blanky/Source/Domain/View/Login/LoginViewModel.swift
+++ Blanky/Blanky/Source/Domain/View/Login/LoginViewModel.swift
@@ -6,7 +6,103 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
-class LoginViewModel {
+class LoginViewModel: BaseViewModel {
+    
+    struct Input {
+        let emailTextField: ControlProperty<String>
+        let passwordTextField: ControlProperty<String>
+        let loginButton: ControlEvent<Void>
+        let findEmailButton: ControlEvent<Void>
+        let findPasswordButton: ControlEvent<Void>
+        let joinButton: ControlEvent<Void>
+    }
+    
+    struct Output {
+        let emailValidation: Driver<Bool> //이메일 유효성 검사
+        let passwordValidation: Driver<Bool> //비밀번호 유효성 검사
+        let finalValidation: Driver<Bool> //모든 유효성 검사 완료했는지 최종 확인
+        let loginButtonTap: Driver<(Bool, String)> //로그인 버튼 탭
+        let findEmailButtonTap: ControlEvent<Void> //이메일 찾기 버튼 탭
+        let findPasswordButtonTap: ControlEvent<Void> //비밀번호 찾기 버튼 탭
+        let joinButtonTap: Driver<Void> //회원가입 버튼 탭
+    }
+    
+    func transform(input: Input) -> Output {
+        
+        //이메일 유효성 검사
+        let emailValidation = input.emailTextField
+            .debounce(.milliseconds(400), scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+            .map { text in
+                let emailRegex = "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
+                guard let _ = text.range(of: emailRegex, options: .regularExpression) else {
+                    return false
+                }
+                return true
+            }
+            .asDriver(onErrorJustReturn: false)
+        
+        //비밀번호 유효성 검사
+        let passwordValidation = input.passwordTextField
+            .debounce(.milliseconds(400), scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+            .map { text in
+                let passwordRegex = "^(?=.*[!_@$%^&+=])[A-Za-z0-9!_@$%^&+=]{8,20}$"
+                guard let _ = text.range(of: passwordRegex, options: .regularExpression) else {
+                    return false
+                }
+                return true
+            }
+            .asDriver(onErrorJustReturn: false)
+        
+        //모든 유효성 검사 완료했는지 최종 확인
+        let finalValidation = Driver.combineLatest(emailValidation, passwordValidation)
+            .map { emailCheck, passwordCheck in
+                return emailCheck && passwordCheck
+            }
+            .asDriver(onErrorJustReturn: false)
+         
+        //로그인 버튼 탭
+        let loginButtonTap = input.loginButton
+            .throttle(.seconds(1), scheduler: MainScheduler.instance)
+            .withLatestFrom(Observable.combineLatest(input.emailTextField, input.passwordTextField))
+            .flatMap { email, password in
+                PostAPIManager.shared.login(email: email, password: password)
+                    .map { result in
+                        switch result {
+                        case .success(let data):
+                            print("로그인 성공: ", data)
+                            return (true, "")
+                        case .failure(let error):
+                            print("로그인 실패: ", error)
+                            return (false, error.errorDescription)
+                        }
+                    }
+            }
+            .asDriver(onErrorJustReturn: (false, ""))
+        
+        //FIXME: 이메일 찾기 버튼 탭
+        //FIXME: 비밀번호 찾기 버튼 탭
+        
+        //회원가입 버튼 탭
+        let joinButtonTap = input.joinButton
+            .throttle(.seconds(1), scheduler: MainScheduler.instance)
+            .asDriver(onErrorJustReturn: ())
+        
+        
+        return Output(
+            emailValidation: emailValidation,
+            passwordValidation: passwordValidation, 
+            finalValidation: finalValidation,
+            loginButtonTap: loginButtonTap,
+            findEmailButtonTap: input.findEmailButton,
+            findPasswordButtonTap: input.findPasswordButton,
+            joinButtonTap: joinButtonTap
+        )
+    }
+    
     
 }
