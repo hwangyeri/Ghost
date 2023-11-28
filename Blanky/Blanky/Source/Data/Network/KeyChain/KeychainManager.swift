8dd562fc7cb2f2aff050f573edb37f9b7e6784bb
hwangyeri <dpfl420@naver.com>
2023-11-28 23:14:26 +0900

    [Feat] Source/Data/Network/KeyChain 그룹으로 폴더링 및 KeychainManager 생성
    - SwiftKeychainWrapper 활용해서 토큰, 리프레시 토큰 키체인에 저장하기 위함

--- Blanky/Blanky/Source/Data/Network/KeyChain/KeychainManager.swift
+++ Blanky/Blanky/Source/Data/Network/KeyChain/KeychainManager.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import Security
+import SwiftKeychainWrapper
 
 final class KeychainManager {
     
@@ -14,5 +14,40 @@ final class KeychainManager {
     
     private init() {}
     
-   
+    private struct KeychainKeys {
+        static let tokenKey: String = "User.Token.Key"
+        static let refreshTokenKey: String = "User.RefreshToken.Key"
+    }
+    
+    var token: String? {
+        get {
+            KeychainWrapper.standard.string(forKey: KeychainKeys.tokenKey) //Keychain 반환
+        }
+        set {
+            if let value = newValue {
+                KeychainWrapper.standard.set(value, forKey: KeychainKeys.tokenKey) //Keychain 저장
+            } else {
+                KeychainWrapper.standard.removeObject(forKey: KeychainKeys.tokenKey) //Keychain 삭제
+            }
+        }
+    }
+    
+    var refreshToken: String? {
+        get {
+            KeychainWrapper.standard.string(forKey: KeychainKeys.refreshTokenKey)
+        }
+        set {
+            if let value = newValue {
+                KeychainWrapper.standard.set(value, forKey: KeychainKeys.refreshTokenKey)
+            } else {
+                KeychainWrapper.standard.removeObject(forKey: KeychainKeys.refreshTokenKey)
+            }
+        }
+    }
+    
+    //모든 키 삭제
+    func removeAllKeys() {
+        KeychainWrapper.standard.removeAllKeys()
+    }
+    
 }
