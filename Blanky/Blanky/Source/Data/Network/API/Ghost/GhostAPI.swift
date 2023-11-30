8d4a1c897aa15dee6a8c2c34f0629e36847b9fce
hwangyeri <dpfl420@naver.com>
2023-11-30 19:50:57 +0900

    [Refactor] GhostAPI 이름 변경, Query String, case postUser, likeMe 추가
    - postRead 수정
    - postUser, likeMe 추가

--- Blanky/Blanky/Source/Data/Network/API/Ghost/GhostAPI.swift
+++ Blanky/Blanky/Source/Data/Network/API/Ghost/GhostAPI.swift
@@ -10,13 +10,15 @@ import Moya
 
 enum GhostAPI {
     case postCreate(model: PostInput) //게시글 작성
-    case postRead(model: PostInput) //게시글 조회
+    case postRead(next: String, limit: String, product_id: String) //게시글 조회
     case postUpdate(model: PostInput, id: String) //게시글 수정
     case postDelete(id: String) //게시글 삭제
+    case postUser(id: String, next: String, limit: String, product_id: String) //유저별 작성한 게시글 조회
     case commentCreate(model: CommentInput, id: String) //댓글 작성
     case commentUpdate(model: CommentInput, id: String, commentID: String) //댓글 수정
     case commentDelete(id: String, commentID: String) //댓글 삭제
     case like(id: String) //좋아요
+    case likeMe(next: String, limit: String) //좋아요한 게시글 조회
 }
 
 extension GhostAPI: TargetType {
@@ -25,22 +27,26 @@ extension GhostAPI: TargetType {
         return URL(string: APIKey.baseURL)!
     }
     
-    var path: String {
+    var path: String { // request.parameter
         switch self {
         case .postCreate, .postRead:
             return "post"
-        // 게시글 수정/삭제 - id
+            // 게시글 수정/삭제 - id
         case .postUpdate(_, let id), .postDelete(let id):
             return "post/:\(id)"
-        // 댓글 작성 - id
+        case .postUser(let id, _, _, _):
+            return "post/user/:\(id)"
+            // 댓글 작성 - id
         case .commentCreate(_, let id):
             return "post/:\(id)/comment"
-        // 댓글 수정/삭제 - id, commentID
+            // 댓글 수정/삭제 - id, commentID
         case .commentUpdate(_, let id, let commentID), .commentDelete(let id, let commentID):
             return "post/:\(id)/comment/:\(commentID)"
-        // 좋아요 - id
+            // 좋아요 - id
         case .like(let id):
             return "post/like/:\(id)"
+        case .likeMe(_, _):
+            return "post/like/me"
         }
     }
     
@@ -49,8 +55,8 @@ extension GhostAPI: TargetType {
         //POST: 게시글/댓글 작성, 좋아요
         case .postCreate, .commentCreate, .like:
             return .post
-        //GET: 게시글 조회
-        case .postRead:
+        //GET: 게시글 조회, 유저별 작성한 게시글 조회
+        case .postRead, .postUser, .likeMe:
             return .get
         //PUT: 게시글/댓글 수정
         case .postUpdate, .commentUpdate:
@@ -61,10 +67,13 @@ extension GhostAPI: TargetType {
         }
     }
     
-    var task: Moya.Task {
+    var task: Moya.Task { //request-query, request-body
         switch self {
-        case .postCreate(let model), .postRead(let model):
+        case .postCreate(let model):
             return .requestJSONEncodable(model)
+        case .postRead(let next, let limit, let product_id), .postUser(_, let next, let limit, let product_id):
+            let parameters: [String: String] = ["next": next, "limit": limit, "product_id": product_id]
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.queryString)
         case .postUpdate(let model, _):
             return .requestJSONEncodable(model)
         case .commentCreate(let model, _):
@@ -73,6 +82,9 @@ extension GhostAPI: TargetType {
             return .requestJSONEncodable(model)
         case .postDelete(_), .like(_), .commentDelete(_, _):
             return .requestPlain
+        case .likeMe(next: let next, limit: let limit):
+            let parameters: [String: String] = ["next": next, "limit": limit]
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.queryString)
         }
     }
     
@@ -82,7 +94,7 @@ extension GhostAPI: TargetType {
             ["Authorization": "\(KeychainManager.shared.token ?? "token error")",
              "Content-Type": "multipart/form-data",
              "SesacKey": APIKey.sesacKey]
-        case .postRead, .postDelete, .commentDelete, .like:
+        case .postRead, .postDelete, .postUser, .commentDelete, .like, .likeMe:
             ["Authorization": "\(KeychainManager.shared.token ?? "token error")",
              "SesacKey": APIKey.sesacKey]
         case .commentCreate, .commentUpdate:
