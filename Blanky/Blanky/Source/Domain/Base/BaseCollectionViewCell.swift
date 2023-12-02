462af18509d8516626973d3fdc2de1ef7f3a8c0f
hwangyeri <dpfl420@naver.com>
2023-12-02 22:25:14 +0900

    [Feat] BaseCollectionViewCell 추가

---
+++ Blanky/Blanky/Source/Domain/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  BaseCollectionViewCell.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/12/01.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureLayout()
+//        self.backgroundColor = UIColor.systemBackground
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() { }
+    
+    func configureLayout() { }
+    
+}
