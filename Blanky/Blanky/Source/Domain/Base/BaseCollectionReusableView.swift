33c9c8e8a8c65eb9615702ee5d612c7cf32e9471
hwangyeri <dpfl420@naver.com>
2023-12-02 22:28:59 +0900

    [Feat] BaseCollectionReusableView 추가

---
+++ Blanky/Blanky/Source/Domain/Base/BaseCollectionReusableView.swift
@@ -0,0 +1,26 @@
+//
+//  BaseCollectionReusableView.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/12/01.
+//
+
+import UIKit
+
+class BaseCollectionReusableView: UICollectionReusableView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() { }
+    
+    func configureLayout() { }
+        
+}
