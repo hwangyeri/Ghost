5e818015855f71432f89ab49643072d1f402509d
hwangyeri <dpfl420@naver.com>
2023-12-03 23:16:13 +0900

    [Feat] NaviBarButton+ setCustomXButton, xButtonTapped 메서드 추가
    - 주석 내용 변경
    - setCustomXButton, xButtonTapped 메서드 추가

--- Blanky/Blanky/Source/Utility/DesignSystem/Extension/NaviBarButton+.swift
+++ Blanky/Blanky/Source/Utility/DesignSystem/Extension/NaviBarButton+.swift
@@ -11,7 +11,7 @@ import Then
 
 extension UIViewController {
     
-    //MARK: Custom NavigationBarItem(Left) - BackButton
+    //MARK: 왼쪽 네비바 버튼, Back Button < 돌아오세요
     func setCustomBackButton() {
         let backButton = GImageButton(imageSize: 18, imageName: "chevron.backward", backgroundColor: .bColor200, tintColor: .white, cornerRadius: 15)
         
@@ -32,7 +32,7 @@ extension UIViewController {
         self.navigationController?.popViewController(animated: true)
     }
     
-    //MARK: Custom NavigationBarItem(Right) - ExitButton 나가기 버튼
+    //MARK: 오른쪽 네비바 버튼, X Button
     func setCustomExitButton() {
         let exitButton = GImageButton(imageSize: 16, imageName: "xmark", backgroundColor: .bColor200, tintColor: .white, cornerRadius: 15)
         
@@ -51,6 +51,27 @@ extension UIViewController {
     //루트뷰로 화면 전환
     @objc func exitButtonTapped() {
         self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    //MARK: 왼쪽 네비바 버튼, X Button
+    func setCustomXButton() {
+        let xButton = GImageButton(imageSize: 16, imageName: "xmark", backgroundColor: .bColor200, tintColor: .white, cornerRadius: 15)
+        
+        xButton.addTarget(self, action: #selector(xButtonTapped), for: .touchUpInside)
+        
+        xButton.snp.makeConstraints { make in
+            make.size.equalTo(44)
+        }
+        
+        
+        let customXButton = UIBarButtonItem(customView: xButton)
+        
+        self.navigationItem.leftBarButtonItem = customXButton
+    }
+    
+    //게시글 작성 취소 버튼
+    @objc func xButtonTapped() {
+        self.navigationController?.popViewController(animated: true)
     }
     
 }
