133f26d0d1e749537476e27aeae5271b050db17d
hwangyeri <dpfl420@naver.com>
2023-12-03 16:48:17 +0900

    [Feat] BaseTableViewCell 추가

---
+++ Blanky/Blanky/Source/Domain/Base/BaseTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  BaseTableViewCell.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/12/03.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureHierarchy()
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func configureHierarchy() { }
+    
+    func configureLayout() { }
+
+}
