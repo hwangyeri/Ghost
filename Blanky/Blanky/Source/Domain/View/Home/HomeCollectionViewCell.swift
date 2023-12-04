45ac7f6b6a9bc3b4372da9debd3309634387a926
hwangyeri <dpfl420@naver.com>
2023-12-04 17:41:34 +0900

    [Feat] HomeCollectionViewCell 생성

---
+++ Blanky/Blanky/Source/Domain/View/Home/HomeCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  HomeCollectionViewCell.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/12/03.
+//
+
+import UIKit
+import SnapKit
+
+class HomeCollectionViewCell: BaseCollectionViewCell {
+    
+    override func configureHierarchy() {
+        
+    }
+    
+    override func configureLayout() {
+        
+    }
+    
+}
