13bb9586b5ea7a4abff273ee57f8414da2256b98
hwangyeri <dpfl420@naver.com>
2023-12-04 17:44:38 +0900

    [Feat] PostCollectionViewCell 생성

---
+++ Blanky/Blanky/Source/Domain/View/Post/PostCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  PostCollectionViewCell.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/12/03.
+//
+
+import UIKit
+import SnapKit
+
+class PostCollectionViewCell: BaseCollectionViewCell {
+    
+    override func configureHierarchy() {
+        
+    }
+    
+    override func configureLayout() {
+        
+    }
+    
+}
