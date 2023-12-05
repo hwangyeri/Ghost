e909417fbb381291efb47bbd7249494c318ba30d
hwangyeri <dpfl420@naver.com>
2023-12-05 17:22:17 +0900

    [Style] PostCollectionViewCell UI 구현

--- Blanky/Blanky/Source/Domain/View/Post/PostCollectionViewCell.swift
+++ Blanky/Blanky/Source/Domain/View/Post/PostCollectionViewCell.swift
@@ -7,15 +7,43 @@
 
 import UIKit
 import SnapKit
+import Then
 
 class PostCollectionViewCell: BaseCollectionViewCell {
     
+    let deleteButton = GImageButton(
+        imageSize: 12,
+        imageName: "xmark",
+        backgroundColor: .bColor200,
+        tintColor: .white,
+        cornerRadius: 15
+    )
+    
+    let imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.layer.masksToBounds = true
+        $0.image = UIImage(systemName: "exclamationmark.icloud")
+        $0.backgroundColor = .bColor100
+        $0.layer.cornerRadius = 25
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.bColor300.cgColor
+    }
+    
     override func configureHierarchy() {
-        
+        [imageView, deleteButton].forEach {
+            self.addSubview($0)
+        }
     }
     
     override func configureLayout() {
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
         
+        deleteButton.snp.makeConstraints { make in
+            make.top.trailing.equalTo(imageView)
+            make.size.equalTo(30)
+        }
     }
     
 }
