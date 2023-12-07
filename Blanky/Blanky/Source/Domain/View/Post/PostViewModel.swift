c719cbf8308d83ed1e8a532a50b3808be7b57a79
hwangyeri <dpfl420@naver.com>
2023-12-07 16:54:54 +0900

    [Feat] 게시글 등록 버튼 눌렀을때 PostAPI 네트워크 통신 (진행중)
    - final 키워드 추가
    - Input: xButton, imageSubject 추가
    - Output: xButtonTap 추가, postButtonTap 타입 변경
    - PostAPI 네트워크 통신: 이미지 데이터로 변환해서 넘겨줘야함

--- Blanky/Blanky/Source/Domain/View/Post/PostViewModel.swift
+++ Blanky/Blanky/Source/Domain/View/Post/PostViewModel.swift
@@ -9,20 +9,32 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-class PostViewModel: BaseViewModel {
+final class PostViewModel: BaseViewModel {
     
     struct Input {
+        let xButton: ControlEvent<Void> //작성 종료 버튼
         let postButton: ControlEvent<Void> //게시글 등록 버튼
         let titleTextField: ControlProperty<String>
         let contentTextView: ControlProperty<String>
+        let imageSubject: Observable<[Data]> //추가한 이미지 배열
     }
     
     struct Output {
+        let xButtonTap: Driver<Void>
+        let postButtonTap: Driver<Bool>
         let textFieldValidation: Driver<Bool> //제목, 내용 텍스트필드 둘다 빈값이 아닌지 확인
-        let postButtonTap: Driver<Void>
     }
     
     func transform(input: Input) -> Output {
+        
+        let temp = input.imageSubject
+            .do { data in
+                print(data)
+            }
+        
+        let xButtonTap = input.xButton
+            .throttle(.seconds(1), scheduler: MainScheduler.instance)
+            .asDriver(onErrorJustReturn: ())
         
         let titleEmpty = input.titleTextField
             .map { !$0.isEmpty }
@@ -37,13 +49,32 @@ class PostViewModel: BaseViewModel {
         
         //게시글 등록 버튼 탭
         let postButtonTap = input.postButton
+            .do(onNext: { _ in
+                print("postButtonTap")
+            })
             .throttle(.seconds(1), scheduler: MainScheduler.instance)
-            .asDriver(onErrorJustReturn: ())
+            .withLatestFrom(Observable.combineLatest(input.titleTextField, input.contentTextView, input.imageSubject))
+            .flatMap { title, content, image in
+                print("---", title, content, image)
+                return PostAPIManager.shared.postCreate(title: title, content: content, file: image)
+                    .map { result in
+                        switch result {
+                        case .success(let data):
+                            print("포스트 작성 성공: ", data)
+                            return true
+                        case .failure(let error):
+                            print("포스트 작성 실패: ", error)
+                            return false
+                        }
+                    }
+            }
+            .asDriver(onErrorJustReturn: false)
         
         
         return Output(
-            textFieldValidation: textFieldValidation,
-            postButtonTap: postButtonTap
+            xButtonTap: xButtonTap, 
+            postButtonTap: postButtonTap,
+            textFieldValidation: textFieldValidation
         )
     }
     
