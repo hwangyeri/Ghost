feff0d8e136bf80cffe761ef0466e109e33a17d5
hwangyeri <dpfl420@naver.com>
2023-12-09 01:21:39 +0900

    [Feat] HomeVC - 조회한 데이터를 가져와서 보여주기 위한 postRead 메서드 추가 (게시글 조회 API)
    - class final 키워드 추가
    - 조회한 데이터를 담을 postDataList 변수 추가
    - 게시글 조회 API 네트워크 통신을 위한 메서드 postRead 생성

--- Blanky/Blanky/Source/Domain/View/Home/HomeViewController.swift
+++ Blanky/Blanky/Source/Domain/View/Home/HomeViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class HomeViewController: BaseViewController {
+final class HomeViewController: BaseViewController {
     
     private let mainView = HomeView()
     
@@ -17,11 +17,7 @@ class HomeViewController: BaseViewController {
     
     let disposeBag = DisposeBag()
     
-//    private let dummy: [PostData] = [
-//        PostData(likes: [], image: [], hashTags: [], comments: [], id: "", creator: Creator(id: "", nick: "nick1", profile: "ghost"), time: "", title: "title1", content: "content1"),
-//        PostData(likes: [], image: [], hashTags: [], comments: [], id: "", creator: Creator(id: "", nick: "nick2", profile: "ghost"), time: "", title: "title2", content: "content2"),
-//        PostData(likes: [], image: [], hashTags: [], comments: [], id: "", creator: Creator(id: "", nick: "nick3", profile: "ghost"), time: "", title: "title3", content: "content3")
-//    ]
+    private var postDataList = PostRead(data: [], next_cursor: "")
     
     override func loadView() {
         self.view = mainView
@@ -29,14 +25,37 @@ class HomeViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         bind()
+        postRead()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+//        postRead()
     }
     
     override func configureLayout() {
         self.navigationItem.hidesBackButton = true
         mainView.tableView.delegate = self
         mainView.tableView.dataSource = self
+    }
+    
+    private func postRead() {
+        // 게시글 조회 API
+        PostAPIManager.shared.postRead(next: "", limit: "20")
+            .subscribe(with: self) { [weak self] _, result in
+                print("네트워크 통신 결과: ", result)
+                switch result {
+                case .success(let data):
+                    print("포스트 조회 성공: ", data)
+                    
+                    self?.postDataList = data
+                    self?.mainView.tableView.reloadData()
+                case .failure(let error):
+                    print("포스트 조회 실패: ", error)
+                }
+            }
+            .disposed(by: disposeBag)
     }
     
     private func bind() {
@@ -63,11 +82,19 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return postDataList.data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.identifier, for: indexPath) as? HomeTableViewCell else { return UITableViewCell()}
+        let row = postDataList.data[indexPath.row]
+        
+        cell.nicknameLabel.text = "익명의유령\(indexPath.row)"
+        cell.dateLabel.text = row.time
+        cell.titleLabel.text = row.title
+        cell.contentLabel.text = row.content
+        cell.messageLabel.text = "\(row.comments.count)"
+        cell.likeLabel.text = "\(row.likes.count)"
        
         return cell
     }
