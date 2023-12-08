a704f20f8ca5e2ee22019610e3fbc6f98a0b2ab5
hwangyeri <dpfl420@naver.com>
2023-12-09 01:29:43 +0900

    [Style] HomeTableViewCell - Layout, UI 변경 및 prepareForReuse 메서드 추가
    - hitsLabel, messageButton, messageLabel, likeButton, likeLabel, bookmarkButton 추가
    - prepareForReuse 메서드 추가
    - stackView 제거
    - awakeFromNib, setSelected 메서드 제거

--- Blanky/Blanky/Source/Domain/View/Home/HomeTableViewCell.swift
+++ Blanky/Blanky/Source/Domain/View/Home/HomeTableViewCell.swift
@@ -13,23 +13,32 @@ final class HomeTableViewCell: BaseTableViewCell {
 
     let profileImageView = UIImageView().then {
         $0.image = UIImage(named: "ghost")
-        $0.layer.cornerRadius = 30
+        $0.layer.cornerRadius = 13
+        $0.layer.borderWidth = 0.5
+        $0.layer.borderColor = UIColor.bColor300.cgColor
+        $0.contentMode = .scaleAspectFit
+        $0.layer.masksToBounds = true
     }
     
     let nicknameLabel = GLabel(
-        text: "nickname",
-        fontWeight: .semiBold,
-        fontSize: .S)
+        text: "익명의유령",
+        fontWeight: .medium,
+        fontSize: .XS)
     
     let dateLabel = GLabel(
-        text: "date",
+        text: "",
         fontWeight: .thin,
-        fontSize: .S)
+        fontSize: .XXS)
+    
+    let titleLabel = GLabel(
+        text: "",
+        fontWeight: .semiBold,
+        fontSize: .M)
     
     let contentLabel = GLabel(
-        text: "content content content content content content content",
-        fontWeight: .semiBold,
-        fontSize: .XS)
+        text: "",
+        fontWeight: .regular,
+        fontSize: .S)
     
     lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout()).then {
         $0.register(HomeCollectionViewCell.self, forCellWithReuseIdentifier: HomeCollectionViewCell.identifier)
@@ -38,11 +47,11 @@ final class HomeTableViewCell: BaseTableViewCell {
     
     private func collectionViewLayout() -> UICollectionViewFlowLayout {
         let layout = UICollectionViewFlowLayout()
-        layout.minimumLineSpacing = 8
-        layout.minimumInteritemSpacing = 8
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        let size = UIScreen.main.bounds.width - 40
-        layout.itemSize = CGSize(width: size / 3, height: size / 3)
+        layout.minimumLineSpacing = 5
+        layout.minimumInteritemSpacing = 5
+        layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+        let size = UIScreen.main.bounds.width / 3.0
+        layout.itemSize = CGSize(width: size - 30, height: 100)
         return layout
     }
     
@@ -64,42 +73,55 @@ final class HomeTableViewCell: BaseTableViewCell {
 //        return layout
 //    }
     
-    let stackView = UIStackView().then {
-        //$0.translatesAutoresizingMaskIntoConstraints = false
-        //$0.isLayoutMarginsRelativeArrangement = true
-        //$0.layoutMargins.left = 15.0
-        $0.axis = .horizontal
-        $0.distribution = .fillEqually
-        $0.spacing = 5
-    }
-    
-    let hitsButton = UIButton().then {
-        let imageConfig = UIImage.SymbolConfiguration(pointSize: 10, weight: .medium)
-        let image = UIImage(systemName: "star", withConfiguration: imageConfig)
-        $0.setImage(image, for: .normal)
-        $0.setTitle("10", for: .normal)
-        $0.tintColor = .white
+    let hitsImage = UIImageView().then {
+        $0.image = UIImage(systemName: "eyes")
         $0.contentMode = .scaleAspectFit
-    }
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        $0.tintColor = .white
     }
     
+    let hitsLabel = GLabel(
+        text: "0",
+        fontWeight: .regular,
+        fontSize: .XS)
+    
+    let messageButton = GImageButton(
+        imageSize: 13,
+        imageName: "message",
+        backgroundColor: .bColor200,
+        tintColor: .white,
+        cornerRadius: 0
+    )
+    
+    let messageLabel = GLabel(
+        text: "0",
+        fontWeight: .regular,
+        fontSize: .XS)
+    
+    let likeButton = GImageButton(
+        imageSize: 15,
+        imageName: "heart",
+        backgroundColor: .bColor200,
+        tintColor: .white,
+        cornerRadius: 0
+    )
+    
+    let likeLabel = GLabel(
+        text: "0",
+        fontWeight: .regular,
+        fontSize: .XS)
+    
+    let bookmarkButton = GImageButton(
+        imageSize: 14,
+        imageName: "bookmark",
+        backgroundColor: .bColor200,
+        tintColor: .white,
+        cornerRadius: 0
+    )
+
     override func configureHierarchy() {
-        [profileImageView, nicknameLabel, dateLabel, contentLabel, collectionView, stackView].forEach {
+        [profileImageView, nicknameLabel, dateLabel, titleLabel, contentLabel, collectionView, 
+         hitsImage, hitsLabel,messageButton, messageLabel, likeButton, likeLabel, bookmarkButton].forEach {
             contentView.addSubview($0)
-        }
-        
-        [hitsButton].forEach {
-            stackView.addArrangedSubview($0)
         }
     }
     
@@ -110,31 +132,87 @@ final class HomeTableViewCell: BaseTableViewCell {
         }
         
         nicknameLabel.snp.makeConstraints { make in
-            make.top.equalTo(profileImageView)
-            make.leading.equalTo(profileImageView.snp.trailing).offset(10)
+            make.centerY.equalTo(profileImageView)
+            make.leading.equalTo(profileImageView.snp.trailing).offset(5)
         }
         
         dateLabel.snp.makeConstraints { make in
-            make.top.equalTo(profileImageView)
+            make.bottom.equalTo(nicknameLabel)
             make.leading.equalTo(nicknameLabel.snp.trailing).offset(5)
+            make.trailing.equalToSuperview().inset(10)
         }
         
-        contentLabel.snp.makeConstraints { make in
-            make.top.equalTo(nicknameLabel.snp.bottom).offset(5)
-            make.leading.equalTo(profileImageView)
-            make.trailing.equalToSuperview()
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(profileImageView.snp.bottom).offset(15)
+            $0.leading.equalTo(profileImageView)
+            $0.trailing.equalToSuperview().inset(10)
+        }
+        
+        contentLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(10)
+            $0.horizontalEdges.equalTo(titleLabel)
+            $0.height.lessThanOrEqualTo(120)
         }
         
         collectionView.snp.makeConstraints { make in
             make.top.equalTo(contentLabel.snp.bottom).offset(10)
-            make.horizontalEdges.equalTo(contentLabel)
+            make.horizontalEdges.equalTo(titleLabel)
             make.height.equalTo(200)
         }
         
-        stackView.snp.makeConstraints { make in
+        hitsImage.snp.makeConstraints { make in
             make.top.equalTo(collectionView.snp.bottom).offset(10)
-            make.horizontalEdges.equalTo(contentLabel)
+            make.leading.equalTo(collectionView).inset(5)
+            make.size.equalTo(16)
+            make.bottom.equalToSuperview().inset(10)
+        }
+        
+        hitsLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(hitsImage)
+            make.leading.equalTo(hitsImage.snp.trailing).offset(5)
+            make.bottom.equalTo(hitsImage)
+        }
+        
+        messageButton.snp.makeConstraints { make in
+            make.top.equalTo(hitsLabel)
+            make.leading.equalTo(hitsLabel.snp.trailing).offset(12)
+            make.bottom.equalTo(hitsImage)
+        }
+        
+        messageLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(messageButton)
+            make.leading.equalTo(messageButton.snp.trailing).offset(5)
+            make.bottom.equalTo(hitsImage)
+        }
+        
+        likeButton.snp.makeConstraints { make in
+            make.top.equalTo(hitsLabel)
+            make.leading.equalTo(messageLabel.snp.trailing).offset(12)
+            make.bottom.equalTo(hitsImage)
+        }
+        
+        likeLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(likeButton)
+            make.leading.equalTo(likeButton.snp.trailing).offset(5)
+            make.bottom.equalTo(hitsImage)
+        }
+        
+        bookmarkButton.snp.makeConstraints { make in
+            make.top.equalTo(hitsImage).offset(-10)
+            make.trailing.equalTo(collectionView).inset(10)
+            make.bottom.equalToSuperview().inset(5)
         }
     }
-
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        nicknameLabel.text = "익명의유령"
+        dateLabel.text = ""
+        titleLabel.text = ""
+        contentLabel.text = ""
+        hitsLabel.text = "0"
+        messageLabel.text = "0"
+        likeLabel.text = "0"
+    }
 }
