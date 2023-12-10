859a89201433430a27bdff3e0f7889cd3c751e6e
hwangyeri <dpfl420@naver.com>
2023-12-10 23:39:14 +0900

    [Feat] GBorderImageView 생성

---
+++ Blanky/Blanky/Source/Utility/DesignSystem/Component/GBorderImageView.swift
@@ -0,0 +1,27 @@
+//
+//  GBorderImageView.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/12/10.
+//
+
+import UIKit
+
+final class GBorderImageView: UIImageView {
+    
+    init(borderWidth: CGFloat, cornerRadius: CGFloat) {
+        super.init(frame: .zero)
+        
+        self.image = UIImage(named: "ghost")
+        self.layer.borderColor = UIColor.bColor300.cgColor
+        self.layer.borderWidth = borderWidth
+        self.layer.cornerRadius = cornerRadius
+        self.contentMode = .scaleAspectFit
+        self.layer.masksToBounds = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
