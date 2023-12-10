8c063374b0e3719aabfe4fcea2c7c27282076cf9
hwangyeri <dpfl420@naver.com>
2023-12-10 21:05:07 +0900

    [Comment] 프린트문 주석 처리

--- Blanky/Blanky/Source/Domain/View/Login/LoginViewModel.swift
+++ Blanky/Blanky/Source/Domain/View/Login/LoginViewModel.swift
@@ -78,8 +78,8 @@ class LoginViewModel: BaseViewModel {
                             //키체인에 토큰 저장
                             KeychainManager.shared.token = data.token
                             KeychainManager.shared.refreshToken = data.refreshToken
-                            print("KeychainManager token: ", KeychainManager.shared.token)
-                            print("KeychainManager refreshToken: ", KeychainManager.shared.refreshToken)
+                            //print("KeychainManager token: ", KeychainManager.shared.token)
+                            //print("KeychainManager refreshToken: ", KeychainManager.shared.refreshToken)
                             return (true, "")
                         case .failure(let error):
                             print("로그인 실패: ", error)
