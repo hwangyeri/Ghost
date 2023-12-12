00bf7799feb3b8e9248fae6bc55f3821be8daeca
hwangyeri <dpfl420@naver.com>
2023-12-13 00:10:45 +0900

    [Feat] Constant 생성
    - authorization, sesacKey 추가

---
+++ Blanky/Blanky/Source/Utility/DesignSystem/Constant/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/12/12.
+//
+
+import Foundation
+
+enum Constant {
+    static let authorization = "Authorization"
+    static let sesacKey = "SesacKey"
+}
