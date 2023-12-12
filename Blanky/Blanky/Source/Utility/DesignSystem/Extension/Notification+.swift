edb5166e0cdb31b843b890a02ec1bf81afccc4cc
hwangyeri <dpfl420@naver.com>
2023-12-12 15:14:38 +0900

    [Feat] Notification+ accessTokenRefreshFailed 추가
    - accessTokenRefreshFailed: 액세스 토큰 만료시 갱신 실패(418) -> 로그인 화면으로 전환
    - 주석 내용 변경

--- Blanky/Blanky/Source/Utility/DesignSystem/Extension/Notification+.swift
+++ Blanky/Blanky/Source/Utility/DesignSystem/Extension/Notification+.swift
@@ -9,11 +9,15 @@ import Foundation
 
 enum AppNotification: String {
     case joinSuccessAlert
+    case accessTokenRefreshFailed
 }
 
 extension NSNotification.Name {
     
-    //회원가입 성공시 루트 뷰로 화면 전환 -> 알럿
+    // 회원가입 성공시 루트 뷰로 화면 전환 -> 알럿
     static let joinSuccessAlert = NSNotification.Name(AppNotification.joinSuccessAlert.rawValue)
+    
+    // 액세스 토큰 만료시 갱신 실패(418) -> 로그인 화면으로 전환
+    static let accessTokenRefreshFailed = NSNotification.Name(AppNotification.accessTokenRefreshFailed.rawValue)
     
 }
