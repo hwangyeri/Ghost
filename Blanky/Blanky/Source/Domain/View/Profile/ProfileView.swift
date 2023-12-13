f23453c254b42788ca161e9acac6b806cc083f25
hwangyeri <dpfl420@naver.com>
2023-12-13 21:13:18 +0900

    [Feat] ProfileView 생성 및 Profile 폴더링

---
+++ Blanky/Blanky/Source/Domain/View/Profile/ProfileView.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileView.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/12/13.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class ProfileView: BaseView {
+    
+    override func configureHierarchy() {
+        
+    }
+    
+    override func configureLayout() {
+        
+    }
+    
+}
