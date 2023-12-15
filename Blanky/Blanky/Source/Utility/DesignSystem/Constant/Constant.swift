ad415fbf714bbd142d85abb5039b08964807610c
hwangyeri <dpfl420@naver.com>
2023-12-16 06:26:15 +0900

    [Feat] Constant contentType, refresh 추가

--- Blanky/Blanky/Source/Utility/DesignSystem/Constant/Constant.swift
+++ Blanky/Blanky/Source/Utility/DesignSystem/Constant/Constant.swift
@@ -11,6 +11,8 @@ enum Constant {
     //API
     static let authorization = "Authorization"
     static let sesacKey = "SesacKey"
+    static let contentType = "Content-Type"
+    static let refresh = "Refresh"
     static let limitNumber = "10"
     
     //Image Name
