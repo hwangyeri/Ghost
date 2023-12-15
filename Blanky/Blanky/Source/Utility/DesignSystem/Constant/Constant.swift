7fd2c7721d32283f21bbfe0215a1840c848895c7
hwangyeri <dpfl420@naver.com>
2023-12-15 23:53:27 +0900

    [Feat] Constant pencil 추가 및 주석 변경

--- Blanky/Blanky/Source/Utility/DesignSystem/Constant/Constant.swift
+++ Blanky/Blanky/Source/Utility/DesignSystem/Constant/Constant.swift
@@ -8,6 +8,14 @@
 import Foundation
 
 enum Constant {
+    //API
     static let authorization = "Authorization"
     static let sesacKey = "SesacKey"
+    static let limitNumber = "10"
+    
+    //Image Name
+    static let heart = "heart"
+    static let message = "message"
+    static let trash = "trash"
+    static let pencil = "pencil"
 }
