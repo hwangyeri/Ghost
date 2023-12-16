c732b9f18489744147aac44dc3b817ce5cc30d73
hwangyeri <dpfl420@naver.com>
2023-12-16 22:30:36 +0900

    [Feat] Notification+ postSuccessAlert 추가, tokenRefreshFailedAlert 제거

--- Blanky/Blanky/Source/Utility/DesignSystem/Extension/Notification+.swift
+++ Blanky/Blanky/Source/Utility/DesignSystem/Extension/Notification+.swift
@@ -9,15 +9,15 @@ import Foundation
 
 enum AppNotification: String {
     case joinSuccessAlert
-    case tokenRefreshFailedAlert
+    case postSuccessAlert
 }
 
 extension NSNotification.Name {
     
-    // 회원가입 성공시 루트 뷰(로그인 화면)로 화면 전환 -> 알럿
+    // 회원가입 성공시 루트뷰(로그인 화면)로 화면 전환 -> 알럿
     static let joinSuccessAlert = NSNotification.Name(AppNotification.joinSuccessAlert.rawValue)
     
-    // 액세스 토큰 만료시 갱신 실패(418) -> 로그인 화면으로 전환 후 알럿
-    static let tokenRefreshFailedAlert = NSNotification.Name(AppNotification.tokenRefreshFailedAlert.rawValue)
+    // 게시글 작성 성공시 루트뷰(홈 화면)로 화면 전환 -> 알럿
+    static let postSuccessAlert = NSNotification.Name(AppNotification.postSuccessAlert.rawValue)
     
 }
