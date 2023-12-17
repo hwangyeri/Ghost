3e3adc007ae23c14b578c85a6820cb789cb3d0d0
hwangyeri <dpfl420@naver.com>
2023-12-17 18:46:37 +0900

    [Feat] ProfileCollectionViewCell 생성

---
+++ Blanky/Blanky/Source/Domain/View/Profile/ProfileCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileCollectionViewCell.swift
+//  Blanky
+//
+//  Created by Yeri Hwang on 2023/12/17.
+//
+
+import UIKit
+import SnapKit
+
+final class ProfileCollectionViewCell: BaseCollectionViewCell {
+    
+}
