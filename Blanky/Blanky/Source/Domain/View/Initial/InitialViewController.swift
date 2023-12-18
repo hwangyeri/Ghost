267d0d1605e394b853136730ec432bd337b72070
hwangyeri <dpfl420@naver.com>
2023-12-18 20:14:55 +0900

    [Comment] InitialVC 주석 추가, 변경

--- Blanky/Blanky/Source/Domain/View/Initial/InitialViewController.swift
+++ Blanky/Blanky/Source/Domain/View/Initial/InitialViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class InitialViewController: UIViewController {
     
-    // TODO: 네트워크 오류 알럿, 런치스크린 로티, 자동 로그인
+    // TODO: 네트워크 오류 알럿, 런치스크린 로티
     
     private let LoginStatus = UserLoginManager.shared.isLogin
     
@@ -65,3 +65,23 @@ final class InitialViewController: UIViewController {
     
 }
 
+/*
+    고민되는 기능
+    1. 홈 화면 - 서치 버튼, 메인 피드에서 베스트 댓글 보여주기, 조회수?
+    2. 탭 추가? 랭킹 뷰, 댓글이나 알림 탭
+    3. 프로필 설정 화면: 내 프로필 수정, 문의 남기기, 알림 설정, 로그아웃, 회원탈퇴
+    4. 답댓글
+    
+    해야하는 기능
+    1. 디테일 뷰 - 이미지 컬렉션뷰
+    2. 프로필 탭 - 작성한 게시글, 좋아요한 게시글 테이블뷰(+헤더뷰)
+    3. 댓글 삭제
+ 
+    구현한 기능
+    - 댓글, 좋아요, 페이지네이션, 게시글 CR, 프로필 조회, 좋아요한 게시글 조회, 로그인, 회원가입, 스크롤 버튼, 토큰 갱신
+ 
+    * 메인 피드 베스트 댓글 보여주기, 랭킹 뷰
+    * 스유 차트 써서 개인 플젝 하나 추가
+    * 쇼핑 앱 찜하기 탭 다른 UI로 구현하기
+ 
+ */
