fad52d6a8b39271b79e6c818f59f5fcfa6cdb552
hwangyeri <dpfl420@naver.com>
2024-02-06 18:18:55 +0900

    [Style] PostCollectionViewCell UI 변경

--- Blanky/Blanky/Source/Domain/View/Post/PostCollectionViewCell.swift
+++ Blanky/Blanky/Source/Domain/View/Post/PostCollectionViewCell.swift
@@ -12,16 +12,16 @@ import Then
 final class PostCollectionViewCell: BaseCollectionViewCell {
     
     let deleteButton = GImageButton(
-        imageSize: 12,
+        imageSize: 10,
         imageName: "xmark",
-        backgroundColor: .bColor200,
+        backgroundColor: .bColor300.withAlphaComponent(0.9),
         tintColor: .white,
-        cornerRadius: 15
+        cornerRadius: 13
     )
     
     let imageView = GBorderImageView(
         borderWidth: 1,
-        cornerRadius: 25
+        cornerRadius: 18
     ).then {
         $0.image = UIImage(systemName: "exclamationmark.icloud")
         $0.backgroundColor = .bColor100
@@ -35,12 +35,13 @@ final class PostCollectionViewCell: BaseCollectionViewCell {
     
     override func configureLayout() {
         imageView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.edges.equalToSuperview().inset(10)
         }
         
         deleteButton.snp.makeConstraints { make in
-            make.top.trailing.equalTo(imageView)
-            make.size.equalTo(30)
+            make.top.equalTo(imageView).offset(-3)
+            make.trailing.equalTo(imageView).offset(3)
+            make.size.equalTo(25)
         }
     }
     
